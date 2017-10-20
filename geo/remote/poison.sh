c9d6bf292e630ee45f8d3cfe256ae34bede3b923
Simon Elliston Ball <simon@simonellistonball.com>
2017-10-20 15:48:32 +0100

    Fixed timestamp in geo poison script

--- geo/remote/poison.sh
+++ geo/remote/poison.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo -e "import time\nprint 'user1,109.252.227.173,'+str(int(time.time()))" | python | /usr/hdp/current/kafka-broker/bin/kafka-console-producer.sh --broker-list $BROKERLIST --topic geohash
+echo -e "import time\nprint 'user1,109.252.227.173,'+str(int(time.time()*1000))" | python | /usr/hdp/current/kafka-broker/bin/kafka-console-producer.sh --broker-list $BROKERLIST --topic geohash
